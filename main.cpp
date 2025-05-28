13fec6dd3c986835b69367d531ff044a1a74df0f
Elkyn Agustin Santos Mendoza <elkynmendoza@unitec.edu>
2025-05-27 20:32:24 -0600

    Adds basic lexer functionality

--- main.cpp
+++ main.cpp
@@ -1,49 +1,28 @@
 #include <iostream>
 #include <string.h>
+#include <fstream>
+#include <sstream>
 #include "SimplLexer.hpp"
 #include "tokens.hpp"
 
-int main(){
-    std::string input = R"( 
-    987 assd
-    /* iden
-    ti fica 92231313dsadsa 
-      */ 
-    ador 123123
-    // this is a comment
-    /
-    /9
-    /a
-    /* THIS IS A LINES  
-    13$#$#$#@$%$% COMMENT */
-    asda dada
-    ++*-
-    -
-    *
-    %
-    ==
-    !=
-    =
-    >=>
-    <=
-    <
-    x = x
-    if
-    else
-    while
-    true
-    false
-    int fn let
-    )";
-    
-    SimplLexer lexer(input);
+int main(int argc, char* argv[]) {
 
-    Token tk = lexer.getNextToken();
+  std::ifstream file(argv[1]);
+  if (!file.is_open()) {
+      std::cout << "Error al abrir el archivo: " << argv[1] << std::endl;
+      return 1;
+  }
 
-
-    while(tk != Token::Eof) {
-        std::cout << lexer.tokenToString(tk) <<" : "<<lexer.text<< std::endl;
-        tk = lexer.getNextToken();
-    }
-    return 0;
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  std::string input = buffer.str();
+  
+  SimplLexer lexer(input);
+  Token tk = lexer.getNextToken();
+  
+  while(tk != Token::Eof) {
+      std::cout << lexer.tokenToString(tk) <<" : "<<lexer.text<< std::endl;
+      tk = lexer.getNextToken();
+  }
+  return 0;
 }
